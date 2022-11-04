24eca4f3ea38c7684238bbc4a7aaa8c0d8aaf13f
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2022-11-04 15:11:39 +0530

    hashmap examples

--- src/main/java/com/example/hashMap/HashMapApplication.java
+++ src/main/java/com/example/hashMap/HashMapApplication.java
@@ -3,11 +3,26 @@ package com.example.hashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class HashMapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HashMapApplication.class, args);
+		HashMap<String,Integer> temp=new HashMap<>();
+		temp.put("John",123);
+		temp.put("Marry",124);
+		temp.put("Carlos",125);
+		System.out.println(temp);
+		System.out.println(temp.get("John"));
+		System.out.println(temp.containsKey("Marry1"));
+		System.out.println(temp.containsValue(123));
+		temp.putIfAbsent("Marry1",126);
+		System.out.println(temp);
+		temp.replace("Marry",1);
+		System.out.println(temp);
+		temp.remove("Marry1");
+		System.out.println(temp);
 	}
 
 }
