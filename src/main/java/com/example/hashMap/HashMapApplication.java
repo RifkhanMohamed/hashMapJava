5c6e4c993fa72d7c7b542dfd0b17574f8432f04b
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2022-11-04 14:53:10 +0530

    first commit

---
+++ src/main/java/com/example/hashMap/HashMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.hashMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashMapApplication.class, args);
+	}
+
+}
